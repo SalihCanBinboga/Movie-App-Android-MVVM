3d4d73aeb2ba0a7b63c1cffd1d30f558ef00b239
salih <salihcn.bnb@gmail.com>
2021-08-14 15:22:05 +0300

    convert to coroutines call

--- app/src/main/java/com/example/movielistandroid/data/remote/MovieService.kt
+++ app/src/main/java/com/example/movielistandroid/data/remote/MovieService.kt
@@ -1,7 +1,6 @@
 package com.example.movielistandroid.data.remote
 
 import MoviesResponseModel
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -10,27 +9,27 @@ import retrofit2.http.Query
 interface MovieService {
 
     @GET("3/movie/now_playing")
-    fun getCurrentPlayingMovies(@Query("api_key") apiKey: String): Call<MoviesResponseModel>
+    suspend fun getCurrentPlayingMovies(@Query("api_key") apiKey: String): MoviesResponseModel
 
 
     @GET("3/movie/upcoming")
-    fun getUpComingMovies(@Query("api_key") apiKey: String): Call<MoviesResponseModel>
+    suspend fun getUpComingMovies(@Query("api_key") apiKey: String): MoviesResponseModel
 
 
     companion object {
         const val MOVIE_API_KEY = "85743f73c2cc3c332a656d70d9663b5c"
 
-        var retrofitService: MovieService? = null
+        var movieService: MovieService? = null
 
-        fun getInstance() : MovieService {
-            if (retrofitService == null) {
+        fun getInstance(): MovieService {
+            if (movieService == null) {
                 val retrofit = Retrofit.Builder()
                     .baseUrl("https://api.themoviedb.org/")
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
-                retrofitService = retrofit.create(MovieService::class.java)
+                movieService = retrofit.create(MovieService::class.java)
             }
-            return retrofitService!!
+            return movieService!!
         }
     }
 }
