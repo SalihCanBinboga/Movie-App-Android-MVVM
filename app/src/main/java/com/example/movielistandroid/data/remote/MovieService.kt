999e4866e8ff476b63b10a71b486eac7fb95d851
salih <salihcn.bnb@gmail.com>
2021-08-14 15:11:54 +0300

    complete singleton retrofit instance snippet

--- app/src/main/java/com/example/movielistandroid/data/remote/MovieService.kt
+++ app/src/main/java/com/example/movielistandroid/data/remote/MovieService.kt
@@ -2,10 +2,12 @@ package com.example.movielistandroid.data.remote
 
 import MoviesResponseModel
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface  MovieService {
+interface MovieService {
 
     @GET("3/movie/now_playing")
     fun getCurrentPlayingMovies(@Query("api_key") apiKey: String): Call<MoviesResponseModel>
@@ -13,4 +15,22 @@ interface  MovieService {
 
     @GET("3/movie/upcoming")
     fun getUpComingMovies(@Query("api_key") apiKey: String): Call<MoviesResponseModel>
+
+
+    companion object {
+        const val MOVIE_API_KEY = "85743f73c2cc3c332a656d70d9663b5c"
+
+        var retrofitService: MovieService? = null
+
+        fun getInstance() : MovieService {
+            if (retrofitService == null) {
+                val retrofit = Retrofit.Builder()
+                    .baseUrl("https://api.themoviedb.org/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                retrofitService = retrofit.create(MovieService::class.java)
+            }
+            return retrofitService!!
+        }
+    }
 }
