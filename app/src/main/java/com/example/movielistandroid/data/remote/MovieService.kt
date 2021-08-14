b0fabbc50db4b1f6ca4fba1179425bd92586d223
salih <salihcn.bnb@gmail.com>
2021-08-14 15:07:10 +0300

    create movie response model

--- app/src/main/java/com/example/movielistandroid/data/remote/MovieService.kt
+++ app/src/main/java/com/example/movielistandroid/data/remote/MovieService.kt
@@ -1,5 +1,6 @@
 package com.example.movielistandroid.data.remote
 
+import MoviesResponseModel
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -7,9 +8,9 @@ import retrofit2.http.Query
 interface  MovieService {
 
     @GET("3/movie/now_playing")
-    fun getCurrentPlayingMovies(@Query("api_key") apiKey: String): Call<String>
+    fun getCurrentPlayingMovies(@Query("api_key") apiKey: String): Call<MoviesResponseModel>
 
 
     @GET("3/movie/upcoming")
-    fun getUpComingMovies(@Query("api_key") apiKey: String): Call<String>
+    fun getUpComingMovies(@Query("api_key") apiKey: String): Call<MoviesResponseModel>
 }
