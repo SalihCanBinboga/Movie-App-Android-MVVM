3f25ed618f32c9a03578008ba489ee46dbb150a7
salih <salihcn.bnb@gmail.com>
2021-08-15 12:57:36 +0300

    active hilt to app

--- app/src/main/java/com/example/movielistandroid/Application.kt
+++ app/src/main/java/com/example/movielistandroid/Application.kt
@@ -1,6 +1,8 @@
 package com.example.movielistandroid
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-class Application : Application() {
-}
+
+@HiltAndroidApp
+class Application : Application() {}
