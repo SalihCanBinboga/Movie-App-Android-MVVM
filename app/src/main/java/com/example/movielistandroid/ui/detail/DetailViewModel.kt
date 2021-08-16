48dd06e470c8f0643e3f84e0b73789507254ccba
salih <salihcn.bnb@gmail.com>
2021-08-16 16:35:47 +0300

    create detail viewmodel and logic methods

---
+++ app/src/main/java/com/example/movielistandroid/ui/detail/DetailViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.movielistandroid.ui.detail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.movielistandroid.data.repositories.MoviesRepository
+import com.example.movielistandroid.utils.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val moviesRepository: MoviesRepository
+) : ViewModel() {
+
+    fun getMovie(movieID: String) = liveData(Dispatchers.IO) {
+        emit(Resource.loading(data = null))
+        try {
+            emit(Resource.success(data = moviesRepository.getMovie(movieID)))
+        } catch (exception: Exception) {
+            emit(Resource.error(data = null, message = exception.message ?: "Error Occurred!"))
+        }
+    }
+}
