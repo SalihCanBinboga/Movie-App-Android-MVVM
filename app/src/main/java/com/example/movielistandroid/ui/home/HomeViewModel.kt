9ad46fac609ac35b55630e482399b02ab9114071
salih <salihcn.bnb@gmail.com>
2021-08-16 14:29:42 +0300

    include to dependency injection architecture

--- app/src/main/java/com/example/movielistandroid/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/movielistandroid/ui/home/HomeViewModel.kt
@@ -1,6 +1,17 @@
 package com.example.movielistandroid.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.movielistandroid.data.repositories.MoviesRepository
+import com.example.movielistandroid.utils.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
 
-class HomeViewModel: ViewModel() {
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val moviesRepository: MoviesRepository
+) : ViewModel() {
+
 }
