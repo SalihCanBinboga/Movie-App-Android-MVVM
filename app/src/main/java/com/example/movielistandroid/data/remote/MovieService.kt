0b43e7eda78781eddf22d90b41ac9c0aa6bf8f80
salih <salihcn.bnb@gmail.com>
2021-08-16 14:26:11 +0300

    movie service import bug fix

--- app/src/main/java/com/example/movielistandroid/data/remote/MovieService.kt
+++ app/src/main/java/com/example/movielistandroid/data/remote/MovieService.kt
@@ -1,6 +1,6 @@
 package com.example.movielistandroid.data.remote
 
-import MoviesResponseModel
+import com.example.movielistandroid.data.models.MoviesResponseModel
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
