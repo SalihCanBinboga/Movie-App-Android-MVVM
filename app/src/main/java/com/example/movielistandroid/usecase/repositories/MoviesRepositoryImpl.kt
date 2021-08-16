d6c3306731a80f2e24f55d64a13c08a8b4614490
salih <salihcn.bnb@gmail.com>
2021-08-16 14:28:00 +0300

    include to dependency injection architecture

--- app/src/main/java/com/example/movielistandroid/usecase/repositories/MoviesRepositoryImpl.kt
+++ app/src/main/java/com/example/movielistandroid/usecase/repositories/MoviesRepositoryImpl.kt
@@ -3,8 +3,13 @@ package com.example.movielistandroid.usecase.repositories
 import com.example.movielistandroid.data.remote.MovieService
 import com.example.movielistandroid.data.remote.MovieService.Companion.MOVIE_API_KEY
 import com.example.movielistandroid.data.repositories.MoviesRepository
+import javax.inject.Inject
 
-class MoviesRepositoryImpl constructor(private val movieService: MovieService) : MoviesRepository {
+class MoviesRepositoryImpl
+@Inject constructor(
+    private val movieService: MovieService
+) : MoviesRepository {
+
     override suspend fun getCurrentPlayingMovies() = movieService.getCurrentPlayingMovies(apiKey = MOVIE_API_KEY)
     override suspend fun getUpComingMovies() = movieService.getUpComingMovies(apiKey = MOVIE_API_KEY);
 }
