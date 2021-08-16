e67018ae5b74c74e7f33f385ab9daa9c98bc6d6e
salih <salihcn.bnb@gmail.com>
2021-08-16 16:36:35 +0300

    create fragment detail skelton

--- app/src/main/java/com/example/movielistandroid/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/movielistandroid/ui/detail/DetailFragment.kt
@@ -5,15 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.example.movielistandroid.R
+import com.example.movielistandroid.data.models.Results
+import com.example.movielistandroid.databinding.FragmentDetailBinding
+import com.example.movielistandroid.databinding.FragmentHomeBinding
+import com.example.movielistandroid.ui.home.HomeViewModel
+import com.example.movielistandroid.utils.Status
 
 class DetailFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    private val detailViewModel: DetailViewModel by viewModels()
+    private val args: DetailFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupObservers()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setupObservers() {
+        detailViewModel.getMovie(movieID = args.movieID).observe(viewLifecycleOwner,{
+            it?.let { resource ->
+                when (resource.status) {
+                    Status.SUCCESS -> {
+                        binding.movieProgressBar.visibility = View.GONE
+                        resource.data?.let { movieResponse -> updateUI(movieResponse = movieResponse) }
+                    }
+                    Status.ERROR -> {
+                        binding.movieProgressBar.visibility = View.GONE
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                    }
+                    Status.LOADING -> {
+                        binding.movieProgressBar.visibility = View.VISIBLE
+                    }
+                }
+            }
+        })
+    }
+
+    private fun updateUI(movieResponse: Results) {
+
+    }
 }
